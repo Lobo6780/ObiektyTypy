32d83a68ea2c36631de3e6756818b81374a5039e
Piotr <75329810+PiotrLobo6780@users.noreply.github.com>
2022-12-07 11:04:09 +0100

    hermetyzacja + metody get i set

--- Termos.cs
+++ Termos.cs
@@ -8,9 +8,33 @@ namespace ConsoleApp7
 {
     class Herbata
     {
-        public string krajPochodzenia;
-        public string nazwa;
-        public string aromat;
+        private string krajPochodzenia;
+        private string nazwa;
+        private string aromat;
+
+        /* tego nie używać
+        public string Aromat
+        {
+            get
+            {
+                return aromat;
+            }
+            set
+            {
+                aromat = value;
+            }
+        }
+        */
+
+        //hermetyzacja
+        public string getkrajPochodzenia()
+        {
+            return krajPochodzenia;
+        }
+        public void setkrajPochodzenia(string kraj)
+        {
+            this.krajPochodzenia = kraj;
+        }
         public void sparz()
         {
             Console.WriteLine("Parzę...");
@@ -18,10 +42,36 @@ namespace ConsoleApp7
     }
     class Termos
     {
-        public int pojemnosc;
-        public int waga;
-        public int cena;
+        private int pojemnosc;
+        private int waga;
+        private double cena;
         public Herbata zawartosc;
+
+        //hermetyzacja
+        public void setPojemnosc(int poj)
+        {
+            this.pojemnosc = poj;
+        }
+        public int getPojemnosc()
+        {
+            return this.pojemnosc;
+        }
+        public void setWaga(int wag)
+        {
+            this.waga = wag;
+        }
+        public int getWaga()
+        {
+            return this.waga;
+        }
+        public void setCena(double cen)
+        {
+            this.cena = cen;
+        }
+        public double getCena()
+        {
+            return this.cena;
+        }
         public Termos(Herbata aherbata)
         {
             this.zawartosc = aherbata;
@@ -40,7 +90,9 @@ namespace ConsoleApp7
         static void Main(string[] args)
         {
             Herbata h1 = new Herbata();
+            h1.setkrajPochodzenia("Kolumbia");
             Termos t1 = new Termos(h1);
+            Console.WriteLine(h1.getkrajPochodzenia());
         }
     }
 }
