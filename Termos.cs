9ac22faed54e217aecbd1319916323b4665007ba
Piotr <75329810+PiotrLobo6780@users.noreply.github.com>
2022-12-07 10:10:43 +0100

    Termos
    Po prostu Termos.

---
+++ Termos.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Termos
+    {
+        public int pojemnosc;
+        public int waga;
+        public int cena;
+        public string zawartosc;
+        private Herbata herbata;
+        public Termos(Herbata aherbata)
+        {
+            this.herbata = aherbata;
+        }
+        public void polej()
+        {
+            Console.WriteLine("Leję...");
+        }
+        public void umyj()
+        {
+            Console.WriteLine("Myję...");
+        }
+    }
+    class Herbata
+    {
+        public string krajPochodzenia;
+        public string nazwa;
+        public string aromat;
+        public void sparz()
+        {
+            Console.WriteLine("Parzę...");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Herbata h1 = new Herbata();
+            Termos t1 = new Termos(h1);
+        }
+    }
+}
